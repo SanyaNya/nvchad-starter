66e12be68e66ecef3cd4f617342f4e07e480b980
SanyaNya <sanyanya4ever@gmail.com>
2024-11-21 13:19:44 +0300

    add PlatformIO plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,4 +19,24 @@ return {
       auto_install = true,
     },
   },
+
+  {
+      "anurag3301/nvim-platformio.lua",
+      dependencies =
+      {
+        { "akinsho/nvim-toggleterm.lua" },
+        { "nvim-telescope/telescope.nvim" },
+        { "nvim-lua/plenary.nvim" }
+      },
+      cmd =
+      {
+        "Pioinit",
+        "Piorun",
+        "Piocmd",
+        "Piolib",
+        "Piomon",
+        "Piodebug",
+        "Piodb",
+      }
+    },
 }
